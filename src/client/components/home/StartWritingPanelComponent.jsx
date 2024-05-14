ad092211af1d2ae2226125d129401156a4e0853b
Tobias Olea <tobiolea97@gmail.com>
2024-05-14 19:15:15 -0300

    Minor adjustments.

--- src/client/components/home/StartWritingPanelComponent.jsx
+++ src/client/components/home/StartWritingPanelComponent.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
-import '../../styles/home.css';
 
 export const StartWritingPanelComponent = () => {
     const navigate = useNavigate();
