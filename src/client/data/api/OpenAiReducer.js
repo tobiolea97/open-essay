a90c24a27e78bfc4b4ee3fa0e4a862926f5edca8
Tobias Olea <tobiolea97@gmail.com>
2024-05-20 08:11:51 -0300

    Displaying an error message when trying to retreive an unexisting writing.

--- src/client/data/api/OpenAiReducer.js
+++ src/client/data/api/OpenAiReducer.js
@@ -31,7 +31,6 @@ export const review = createAsyncThunk('/essay/review', async (params) => {
         }
 
         let feedback = await response.json();
-        debugger;
         let writing = {
             "writing": params.inputValue,
             "assignmentId": params.assignmentId,
